65345f93eecfb9e6f5a0e1505a5dec2321fe4e50
YY <youtbecyycjs@gmail.com>
2024-08-24 15:40:40 +0800

    /test

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/test", (req, res) => {
     res.render("index");
 });
 
