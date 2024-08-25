31e734cb2cb0374d41210679a2cf2d33acd85b55
YY <youtbecyycjs@gmail.com>
2024-08-25 16:31:20 +0800

    testing css error

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(helmet({
 const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
